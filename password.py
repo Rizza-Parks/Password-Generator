427bbb859766cc1de10ccfe0a66f644bc9636b43
Rizza Parks <cadegarlett@gmail.com>
2025-02-12 19:34:58 +0000

    replaced letters with symbols

--- password.py
+++ password.py
@@ -24,18 +24,25 @@ def fetch_word():
     word =response.json()[0]
     return word
 
-def replaceLetters():
+def replaceLetters(word):
     word = word[0].upper() + word[1:]
 
     if "a" in word:
-        word = word.replace("a," "@")
+        word = word.replace("a", "@")
+
+    if "l" in word:
+        word = word.replace("l", "|")
+
+    if "s" in word:
+        word = word.replace("s", "$")   
 
     return word
-
     
 def generate_weaker_password():
     word1 = fetch_word()
     word2 = fetch_word()
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
     password = word1 + word2
     return password
 
